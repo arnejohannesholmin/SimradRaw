c51e3033dcc2bb1a9fef6367396232013d460589
Arne Johannes Holmin <arnejh@imr.no>
2018-10-31 10:32:56 +0100

    Up to date

--- R/readEKRaw.R
+++ R/readEKRaw.R
@@ -36,6 +36,27 @@ readEKRaw <- function(f, t=1, endian="little", timeOffset=0, drop.out=FALSE, msg
 		c(x, TSD::NAs(len - length(x)))[seq_len(len)]
 	}
 	
+	# Function for removing trailing NAs for all beams in a ping of acoustic data:
+	stripNAsAtBeamEnd <- function(x){
+		allNA <- apply(is.na(x), 1, all)
+		if(any(allNA)){
+			# Only strip from the end of the beam:
+			if(allNA[length(allNA)]){
+				# Get the position where the reversed allNA moves fromTRUE to FALSE (diff -1):
+				posFromEnd <- which(diff(rev(allNA)) == -1)
+				# Return NULL in th unlikely event that all values are NA:
+				if(length(posFromEnd)==0){
+					return(NULL)
+				}
+				posFromEnd <- min(posFromEnd)
+				# Get the sequence to extract samples along the beam by:
+				valid <- seq_len(length(allNA) - posFromEnd)
+				x <- x[valid, , drop=FALSE]
+			}
+		}
+		x
+	}
+	
 	# Function for splitting an array by its last dimension (e.g. representing time), and keeping the first dimensions:
 	splitByLastDim <- function(x, ind){
 		dimx <- dim(x)
@@ -45,9 +66,13 @@ readEKRaw <- function(f, t=1, endian="little", timeOffset=0, drop.out=FALSE, msg
 			out <- as.list(x)
 		}
 		else{
+			# Create a split vector and split by the last dimension:
 			ind <- rep(seq_len(lastdim), each=prod(firstdim))
 			out <- split(x, ind)
+			# Convert all pings to matrices:
 			out <- lapply(out, array, dim=firstdim)
+			# Strip off NAs:
+			out <- lapply(out, stripNAsAtBeamEnd)
 		}
 	
 		out
@@ -413,4 +438,229 @@ readEKRaw <- function(f, t=1, endian="little", timeOffset=0, drop.out=FALSE, msg
 	list(header=header, data=data, numt=numt, numb=numb, dg=temp$dg)
 	##################################################
 	##################################################
+}
+
+# Function for converting mode_high and mode_low to mode:
+readEKRaw_getMode <- function(x, ...){
+	if(length(x$mode_high) && length(x$mode_high)){
+		x$mode <- 256 * x$mode_high + x$mode_low
+		# Remove the mode components:
+		x$mode_high <- NULL
+		x$mode_low <- NULL
+	}
+	x
+}
+# Function for converting fishery sonar data in complex form to power:
+readEKRaw_complex2power <- function(x, complex.out=FALSE, ...){
+	if(length(x$data)){
+		dim(x$data) <- c(2, x$ncomplexpersample, x$count)
+		# Save as complex type:
+		x$data <- complex(real=x$data[1,,], imaginary=x$data[2,,])
+		dim(x$data) <- c(x$ncomplexpersample, x$count)
+		# Sum over the number of complex values per sample, and take the square of the abs, which is the same as the sum of the real part squared and the imaginary part squared:
+		x$power <- 10*log10(abs(colSums(x$data))^2)
+		
+		# Remove the complex
+		if(!complex.out){
+			x$data <- NULL
+		}
+	}
+	x
+}
+# Function for converting getting athwartship and alongship angles:
+readEKRaw_getAngles <- function(x, ...){
+	if(length(x$angle)){
+		x$angle <- matrix(angle, nrow=x$count, ncol=2, byrow=TRUE)
+		x$athwartship <- x$angle[,1]
+		x$alongship <- x$angle[,2]
+		# Remove the angle
+		x$angle <- NULL
+	}
+	x
+}
+# Function for converting getting athwartship and alongship angles:
+readEKRaw_getPower <- function(x, ...){
+	if(length(x$power0)){
+		# Power * 10 * log10(2) / 256
+		x$power <- x$power0 * 0.011758984205624
+		# Remove the power0
+		x$power0 <- NULL
+	}
+	x
+}
+
+readEKRaw_getEnvironment <- function(x){
+	if(length(x$XML0)){
+		atEnvironment <- which(sapply(x$XML0, function(x) length(x$.attrs["SoundSpeed"])>0))
+		x$environment <- as.list(x$XML0[[atEnvironment]]$.attrs)
+		x$environment <- lapply(x$environment, as.numeric)
+		x$environment <- as.data.frame(x$environment)
+		# Remove the envitonment form the list of XML0 datagrams:
+		#x$XML0[[atEnvironment]] <- NULL
+	}
+	x
+}
+
+readEKRaw_removeFirst <- function(data, name){
+	if(length(data[[name]])==1){
+		data[[name]] <- NULL
+	}
+	else{
+		data[[name]] <- data[[name]][-1]
+	}
+	data
+}
+
+readEKRaw_UsedXML0 <- function(data){
+	atTransceivers <- which(sapply(data$XML0, function(x) "Transceivers" %in% names(x)))[1]
+	atEnvironment <- which(sapply(data$XML0, function(x) "SoundSpeed" %in% names(x$.attrs)))[1]
+	atVersion <- which(sapply(data$XML0, function(x) "version" %in% names(x)))[1]
+	
+	data$XML0 <- data$XML0[-c(atTransceivers, atEnvironment, atVersion)]
+	
+	data
+}
+
+readEKRaw_GetRawFileFormat <- function(x){
+	# The RAW0-fileformat contains both CON0 and RAW0:
+	if(all(c("CON0", "RAW0") %in% x$dgName)){
+		rawFileFormat <- 0
+	}
+	else if(all(c("CON0", "RAW1") %in% x$dgName)){
+		rawFileFormat <- 1
+	}
+	else if(all(c("RAW2") %in% x$dgName)){
+		rawFileFormat <- 2
+	}
+	else if("XML0" %in% x$dgName && any(c("RAW0", "RAW3") %in% x$dgName)){
+		rawFileFormat <- 3
+	}
+	else{
+		warning("Unsupported rawFileFormat")
+	}
+	rawFileFormat
+}
+
+readEKRaw_getDataConfig <- function(config, rawFileFormat){
+	#if(length(config$transceiver)){
+	#	out <- config$transceiver
+	#}
+	#else if(length(config$Transceivers)){
+	#	out <- getDataConfigRaw3(config)
+	#}
+	if(rawFileFormat %in% 0:1){
+		out <- config$transceiver
+	}
+	else if(rawFileFormat == 3){
+		out <- getDataConfigRaw3(config)
+	}
+	else{
+		warning("Unsupported rawFileFormat")
+	}
+	out
+}
+
+readEKRaw_getHeader <- function(config, data, rawFileFormat){
+	if(rawFileFormat %in% 0:1){
+		header <- config$header
+	}
+	else if(rawFileFormat == 3){
+		header <- getHeaderRaw3(config=config, data=data)
+	}
+	else{
+		warning("Unsupported rawFileFormat")
+	}
+	header	
+}
+
+
+splitByChar <- function(x, char=";", numeric=TRUE){
+	if(is.character(x)){
+		x <- strsplit(x, char)[[1]]
+		if(numeric){
+			x <- as.numeric(x)
+		}
+	}
+	x
+}
+getTransducerVarOne <- function(x, var){
+	if(is.list(x)){
+		out <- x$.attrs[var]
+	}
+	else{
+		out <- x[var]
+	}
+	out <- splitByChar(unname(out))
+	out
+}
+getTransducerVar <- function(config, var){
+	out <- lapply(config$Transceivers, function(x) getTransducerVarOne(x$Channels$Channel$Transducer, var))
+	if(length(out[[1]]) == 1){
+		out <- unname(unlist(out))
+	}
+	else{
+		out <- do.call(rbind, out)
+	}
+	out
+}
+getFrequencyPar <- function(config){
+	getFrequencyParOne <- function(x){
+		out <- x$Channels$Channel$Transducer
+		out <- out[names(out) %in% "FrequencyPar"]
+		if(length(out)){
+			out <- lapply(out, function(x) {storage.mode(x) <- "numeric"; x})
+			out <- do.call(rbind, out)
+			rownames(out) <- NULL
+		}
+		else{
+			out <- NA
+		}
+		out
+	}
+	out <- lapply(config$Transceivers, getFrequencyParOne)
+	out <- unname(out)
+	out
+}
+getDataConfigRaw3 <- function(config){
+	out <- list(
+		channelid                   = sapply(config$Transceivers, function(x) x$Channels$Channel$.attrs["ChannelID"]),
+		beamtype                    = getTransducerVar(config, "BeamType"),
+		frequency                   = getTransducerVar(config, "Frequency"),
+		gain                        = getTransducerVar(config, "Gain"),
+		equivalentbeamangle         = getTransducerVar(config, "EquivalentBeamAngle"),
+		beamwidthalongship          = getTransducerVar(config, "BeamWidthAlongship"),
+		beamwidthathwartship        = getTransducerVar(config, "BeamWidthAthwartship"),
+		anglesensitivityalongship   = getTransducerVar(config, "AngleSensitivityAlongship"),
+		anglesensitivityathwartship = getTransducerVar(config, "AngleSensitivityAthwartship"),
+		anglesoffsetalongship       = getTransducerVar(config, "AngleOffsetAlongship"),
+		angleoffsetathwartship      = getTransducerVar(config, "AngleOffsetAthwartship"),
+		posx                        = NA,
+		posy                        = NA,
+		posz                        = NA,
+		dirx                        = NA,
+		diry                        = NA,
+		dirz                        = NA,
+		pulselengthtable            = do.call(rbind, lapply(config$Transceivers, function(x) splitByChar(x$Channels$Channel$.attrs["PulseDuration"]))),
+		spare2                      = NA,
+		gaintable                   = getTransducerVar(config, "Gain"),
+		spare3                      = NA,
+		sacorrectiontable           = getTransducerVar(config, "SaCorrection"),
+		spare4                      = NA
+	)
+	# Add the FrequencyPar:
+	out$FrequencyPar <- getFrequencyPar(config)
+	out
+}
+getHeaderRaw3 <- function(data, config){
+	# Combine the header of the Transceivers element in the (should be first) XML0 datagram, and the version (usually third) XML0 datagram:
+	atTransceivers <- which(sapply(data$XML0, function(x) "Transceivers" %in% names(x)))[1]
+	atversion <- which(sapply(data$XML0, function(x) "version" %in% names(x)))[1]
+	out <- c(
+		data$XML0[[atTransceivers]]$Header, 
+		data$XML0[[atversion]] 
+	)
+	
+	# Remove the time stamp:
+	out <- out[tolower(names(out)) != "time"]
+	out
 }
